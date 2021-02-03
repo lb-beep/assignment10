c33e4d1bde7b8a5611588d41196b5a9aedb250ae
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-02-03 08:50:03 -0700

    Add files via upload

---
+++ BakeryShoppingCart 3/BakeryShoppingCart/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BakeryShoppingCart.Models;
+using BakeryShoppingCart.DTOS;
+using BakeryShoppingCart.Exceptions;
+using BakeryShoppingCart.Repositories.Implementation;
+
+namespace BakeryShoppingCart
+{
+    class Program
+    {
+        static void Main()
+        {
+            UserRespository userRepo = new UserRespository();
+            
+
+            User newUser = new User();
+            newUser.Email = "luis.email.com";
+            newUser.UserId = 1;
+            newUser.UserName = "Luis";
+
+            User newUser2 = new User();
+            newUser2.Email = "myra.email.com";
+            newUser2.UserId = 2;
+            newUser2.UserName = "Myra";
+
+            User newUser3 = new User();
+            newUser3.Email = "Luis.email.com";
+            newUser3.UserId = 3;
+            newUser3.UserName = "Luis";
+
+            userRepo.Save(newUser);
+            userRepo.Save(newUser2);
+            userRepo.Save(newUser3);
+
+            userRepo.GetAllUsersByName("2");
+
+        }
+    }
+}
